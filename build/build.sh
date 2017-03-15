9d838f1f9bac6c38b26c3657965872f437a15954
Noah Mcaulay <mcaulayn@perudyptes.cs.pdx.edu>
2017-03-14 17:01:34 -0700

    Modified build script for creating release/debug exes. Got profile data and got output from gprof.

--- build/build.sh
+++ build/build.sh
@@ -5,12 +5,16 @@ cd vic/drivers/classic/
 
 echo "== Building release version"
 make clean
+mv Makefile.release Makefile
 make 
 mv vic_classic.exe ../../../../run/
+mv Makefile Makefile.release
 
 echo "== Building debug version for profiling"
-make -f MakefileDebug clean
-make -f MakefileDebug
-mv vic_classic.exe ../../../../run/vic_classic_debug.exe
+make clean
+mv Makefile.debug Makefile
+make
+mv vic_classic_debug.exe ../../../../run/
+mv Makefile Makefile.debug
 
 echo "Done."
